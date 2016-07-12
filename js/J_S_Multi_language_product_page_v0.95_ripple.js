a5982cd05487d6f6cc1a122dad58fc13410d3205
darknessrain <fvt.jerry.shih@gmail.com>
2016-07-13 01:13:53 +0800

    Add files via upload

--- js/J_S_Multi_language_product_page_v0.95_ripple.js
+++ js/J_S_Multi_language_product_page_v0.95_ripple.js
@@ -47,7 +47,7 @@ function SwitchFromTabCode(){
     }
     return query_string;
   }();
-  console.log(query_string.tabecode);
+  console.log(query_string.tabcode);
   if(query_string.tabcode!=null){
 
       changeBSTab(query_string.tabcode);
