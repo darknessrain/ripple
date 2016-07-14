60c969a639ee4b26633a8454f6a70b36f90a6248
darknessrain <fvt.jerry.shih@gmail.com>
2016-07-14 19:00:12 +0800

    Add files via upload

--- js/J_S_Multi_language_product_page_v0.95_ripple.js
+++ js/J_S_Multi_language_product_page_v0.95_ripple.js
@@ -132,7 +132,31 @@ function changeNavBarUIWording(lang_index){
   return;
 }
 
+//設定cookie的function
+function setCookie(cookieName, cookieValue, exdays) {
+  if (document.cookie.indexOf(cookieName) >= 0) {
+    var expD = new Date();
+    expD.setTime(expD.getTime() + (-1*24*60*60*1000));
+    var uexpires = "expires="+expD.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + "; " + uexpires+"; "+ 'path=/';
+  }
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + "; " + expires+"; "+ 'path=/';
+}
 
+// 讀取cookie
+function getCookie(cookieName) {
+  var name = cookieName + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1);
+      if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
 
 
 
